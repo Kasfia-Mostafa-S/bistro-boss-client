ea15ba6024ccdd516fbc6f6b2dcb898aeff5727c
Kasfia-Mostafa-S <kasfiamostafa.s@gmail.com>
2024-03-29 13:34:02 +0600

    created admin routes

--- src/Router/AdminRoutes.jsx
+++ src/Router/AdminRoutes.jsx
@@ -1,17 +1,20 @@
 import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../Hooks/useAdmin"
 import useAuth from "../Hooks/useAuth";
 
-const PrivateRoutes = ({ children }) => {
-  
-const { user ,loading} = useAuth();
+const AdminRoutes = () => {
+  const { user ,loading} = useAuth();
   const location = useLocation()
-  if(loading){
+  const [isAdmin,isAdminLoading]=useAdmin()
+
+  if(loading || isAdminLoading){
     <progress className="progress w-56"></progress>
   }
-  if (user) {
+  if (user && isAdmin) {
     return children;
   }
   return <Navigate to='/login' state={{from:location}} replace></Navigate>
 };
+}
 
-export default PrivateRoutes;
+export default AdminRoutes
